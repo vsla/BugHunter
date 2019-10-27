000ff7e4a250883362b0311d531b32ca2587744f
victor <victor.sena@melhorcom.com.br>
2019-10-27 14:34:07 -0300

    pegando projetos, mas faltando atributos

--- src/app/pages/BugHunter/pages/MainPage/DashProjects.jsx
+++ src/app/pages/BugHunter/pages/MainPage/DashProjects.jsx
@@ -1,25 +1,31 @@
-import React, { Component, Fragment } from 'react';
+import React, { Component, Fragment } from "react";
 import {
-  Grid, withStyles, createStyles, Divider, Typography,
-} from '@material-ui/core';
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import { Link } from 'react-router-dom';
+  Grid,
+  withStyles,
+  createStyles,
+  Divider,
+  Typography,
+  CircularProgress
+} from "@material-ui/core";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import { Link } from "react-router-dom";
+import ProjectService from "app/services/ProjectService";
 
 const styles = createStyles({
   paper: {
-    maxHeight: '100%',
-    width: '100%',
+    maxHeight: "100%",
+    width: "100%"
   },
   category: {
-    padding: '5px 8px',
-    border: '1px solid #47525E',
-    borderRadius: 8,
+    padding: "5px 8px",
+    border: "1px solid #47525E",
+    borderRadius: 8
   },
   link: {
-    display: 'inline',
-    '&:hover': {
-      textDecoration: 'underline',
+    display: "inline",
+    "&:hover": {
+      textDecoration: "underline"
     }
   }
 });
@@ -29,51 +35,76 @@ class DashProjects extends Component {
     super(props);
 
     this.state = {
-      projects: [1, 2, 3, 4, 5, 6, 7, 8, 9],
-
+      projects: []
     };
   }
 
+  async componentDidMount() {
+    const response = await ProjectService.getAllProjects();
+    this.setState({ projects: response.data });
+    console.log(response);
+  }
+
+  renderProjects = () => {
+    const { projects } = this.state;
+    const { classes } = this.props;
+    if (projects.length > 0) {
+      return projects.map(({ name, description }) => (
+        <>
+          <ListItem>
+            <Grid container spacing={1} style={{ paddingTop: 5 }}>
+              <Grid item xs={12}>
+                <Link to="#">
+                  <Typography
+                    className={classes.link}
+                    variant="h3"
+                    color="primary"
+                  >
+                    {name}
+                  </Typography>
+                </Link>
+              </Grid>
+              <Grid item xs={12}>
+                <Typography>{description}</Typography>
+              </Grid>
+              <Grid item xs={12}>
+                <Grid container direction="row" alignItems="center" spacing={4}>
+                  <Grid item>
+                    <Typography>R$ 2.0X</Typography>
+                  </Grid>
+                  <Grid item>
+                    <Typography className={classes.category}>
+                      Desktop
+                    </Typography>
+                  </Grid>
+                </Grid>
+              </Grid>
+            </Grid>
+          </ListItem>
+          <Divider />
+        </>
+      ));
+    } else {
+      return (
+        <Grid container justify='center'>
+          <Grid item>
+            <CircularProgress />
+          </Grid>
+        </Grid>
+      );
+    }
+  };
 
   render() {
     const { classes } = this.props;
-    const { projects } = this.state;
-    return (
-      <List component="nav" className={classes.paper} aria-label="main mailbox folders">
-        {
-          projects.map(() => (
-            <>
-              <ListItem>
-                <Grid container spacing={1} style={{ paddingTop: 5 }}>
-                  <Grid item xs={12}>
-                    <Link to="#" >
-                      <Typography className={classes.link} variant="h3" color="primary">Título do projeto</Typography>
-                    </Link>
-                  </Grid>
-                  <Grid item xs={12}>
-                    <Typography>
-                      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                      Phasellus posuere purus sed metus
-                      eleifend elementum. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                    </Typography>
-                  </Grid>
-                  <Grid item xs={12}>
-                    <Grid container direction="row" alignItems="center" spacing={4}>
-                      <Grid item>
-                        <Typography>R$ 2.0X</Typography>
-                      </Grid>
-                      <Grid item>
-                        <Typography className={classes.category}>Desktop</Typography>
-                      </Grid>
-                    </Grid>
 
-                  </Grid>
-                </Grid>
-              </ListItem>
-              <Divider />
-            </>
-          ))
-        }
+    return (
+      <List
+        component="nav"
+        className={classes.paper}
+        aria-label="main mailbox folders"
+      >
+        {this.renderProjects()}
       </List>
     );
   }
