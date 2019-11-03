473448a087be1fb9b962f461a389cfcb662a1dab
victor <victor.sena@melhorcom.com.br>
2019-11-03 12:34:47 -0300

    Updates

---
+++ precache-manifest.1cd589e3216e293f7673be239ee4d7b6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "132574ef429222b1735a68b98597aaaf",
+    "url": "/BugHunter/index.html"
+  },
+  {
+    "revision": "bc41d4af1d8f31256519",
+    "url": "/BugHunter/static/js/2.b9ca2a14.chunk.js"
+  },
+  {
+    "revision": "f54f28a3bb3186dce8b3",
+    "url": "/BugHunter/static/js/main.ab223b78.chunk.js"
+  },
+  {
+    "revision": "e2646b16d566d5112b93",
+    "url": "/BugHunter/static/js/runtime-main.adb59787.js"
+  },
+  {
+    "revision": "0570b2be23cf51a70506a930d3e1dc75",
+    "url": "/BugHunter/static/media/sadface.0570b2be.png"
+  }
+]);
